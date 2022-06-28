3f1260ea3005a1098de02c6b8a6ed9d23366474f
Henriquesoto92 <henriquesoto92@gmail.com>
2022-06-28 17:53:22 -0300

    :construction: Add new Features in Users

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,40 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as S from './style';
 import {
     iconShow,
 } from '../../assets';
 
-//https://getbootstrap.com.br/docs/4.1/components/pagination/
-
 const Pagination: React.FC = () => {
+    const [page, setPage] = useState<number>(1);
+
     return (
-        <nav aria-label="Navegação de página exemplo">
-            <ul className="pagination">
-                <li className="page-item">
-                    <a className="page-link" href="#" aria-label="Anterior">
-                        <span aria-hidden="true"><img src={iconShow} alt="Anterior" /></span>
-                        <span className="sr-only">Anterior</span>
-                    </a>
-                </li>
-                <li className="page-item">
-                    <a className="page-link" href="#">1</a>
-                </li>
-                <li className="page-item">
-                    <a className="page-link" href="#">2</a>
-                </li>
-                <li className="page-item">
-                    <a className="page-link" href="#">3</a>
-                </li>
-                <li className="page-item">
-                    <a className="page-link" href="#">4</a>
-                </li>
-                <li className="page-item">
-                    <a className="page-link" href="#" aria-label="Próximo">
-                        <span aria-hidden="true"><img src={iconShow} alt="Próximo" /></span>
-                        <span className="sr-only">Próximo</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+        <S.ContainerBtn>
+            <S.PrevNext
+                to="prev"
+                onClick={() => {
+                    let cont = page - 1;
+                    setPage(cont);
+                }}
+                disabled={page === 1 ? true : false}
+            >
+                <img src={iconShow} alt="" />
+            </S.PrevNext>
+            <S.AtualPage>{page}</S.AtualPage>
+            <S.Page>{page + 1}</S.Page>
+            <S.Page>{page + 2}</S.Page>
+            <S.Page>{page + 3}</S.Page>
+            <S.PrevNext
+                to="next"
+                onClick={() => {
+                    let cont = page + 1;
+                    setPage(cont);
+                }}
+            >
+                <img src={iconShow} alt="" />
+            </S.PrevNext>
+        </S.ContainerBtn>
     )
 };
 
