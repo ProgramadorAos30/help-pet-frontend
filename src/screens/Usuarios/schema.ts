229a3aedfd510ed683a654b4c1d9e77f33f7f1ce
henriquesoto92 <henriquesoto92@gmail.com>
2022-07-06 11:41:45 -0300

    alteração no schema do usuario

--- src/screens/Usuarios/schema.ts
+++ src/screens/Usuarios/schema.ts
@@ -6,5 +6,5 @@ export const schema = Yup.object().shape({
     email: Yup.string().required("Digite um E-mail valido").email(),
     state: Yup.string().required("Selecione um Estado"),
     city: Yup.string().required("Selecione uma Cidade"),
-    password: Yup.string().required("A senha deve ter no minimo 6 dígitos").min(6),
+    password: Yup.string().required("A senha deve ter no minimo 6 dígitos").min(6, "A senha deve ter no minimo 6 dígitos"),
 });
