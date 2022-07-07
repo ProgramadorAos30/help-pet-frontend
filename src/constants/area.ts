f136357738a29cd4842e92f6d77522f23ad1d183
gabrielbarbosaa <gbarbosa1602@gmail.com>
2022-07-07 15:10:54 -0300

    fix: Register occurrence

--- src/constants/area.ts
+++ src/constants/area.ts
@@ -4,5 +4,6 @@ export const AREA = [
     { label: 'Ocupação', value: 'Occupation' },
     { label: 'Quiombo', value: 'Quilombo' },
     { label: 'Vila', value: 'Village' },
-    { label: 'Povoado', value: 'Settlement' }
+    { label: 'Povoado', value: 'Settlement' },
+    { label: 'Outro', value: 'Other' }
 ];
